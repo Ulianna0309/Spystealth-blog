67c518af56aa1c3eca19d99bc60f9f7911b2af22
yulia_vishnevskaya <yulia_vishnevskaya@tchdm.live>
2022-03-24 22:48:14 +0200

    start contact form

--- index.php
+++ index.php
@@ -336,7 +336,15 @@
             <p class="post__text black">
                 Want to know which websites saw the most traffic growth in your industry? Not sure why your SEO strategy doesn’t work? Or simply looking for SEO tips? Subscribe to our newsletter to receive updates on the content you care about.
             </p>
-             <form class="form" method="post" data-form action="#">
+            <?php echo do_shortcode('[contact-form-7 id="155" title="Subscribe form"]'); ?>
+            <label class="form-label black">By clicking “Subscribe” you agree to Spystealth
+                    <a class="white__link" target="_blank" href="#">Privacy Policy</a>
+                    and consent to Spystealth using your contact data for newsletter purposes
+                </label>
+
+                <?php echo do_shortcode('[contact-form-7 id="154" title="Контактная форма 1"]'); ?>
+             <!-- <form class="form" method="post" data-form action="#">
+                <label for="email"></label>
                 <input class="form-input" type="email" name="email" id="email" placeholder="Your email" data-validate-field>
                 <span class="hidden form-error" data-input-error>Invalid value</span>
                 <button class="form-button button__subscribe" type="button" data-submit>Subscribe</button>
@@ -345,7 +353,7 @@
                     and consent to Spystealth using your contact data for newsletter purposes
                 </label>
                 <p class="hidden form-message" data-message>You are subscribed to our newsletter</p>
-            </form>
+            </form> -->
         </div>
         <img class="main-form__img v-col-lg-5" src="<?php echo bloginfo('template_url'); ?>/assets/img/anytime.png" alt="anytime" loading="lazy">
     </div>

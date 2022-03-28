bfa7e0432e39fe138bb873e8ba6b9d7eb52f5521
yulia_vishnevskaya <yulia_vishnevskaya@tchdm.live>
2022-03-28 17:13:32 +0300

    fiinish category

--- archive.php
+++ archive.php
@@ -7,6 +7,46 @@ $category = get_queried_object();
             get_template_part( 'template-parts/category/category-hero');
             get_template_part( 'template-parts/category/category-posts');
         ?>
+
+        <section class="boost container v-row">
+            <?php 
+                $posts = get_posts( array(
+                    'numberposts' => 1,
+                    'category_name'    => 'boost',
+                    'orderby'     => 'date',
+                    'order'       => 'ASC',
+                    'post_type'   => 'post',
+                    'suppress_filters' => true, // подавление работы фильтров изменения SQL запроса
+                ) );
+
+                foreach( $posts as $post ){
+                    setup_postdata($post);
+                    ?>
+
+                    <section class="boost container v-row">
+                        <div class="boost__img v-col-lg-6 v-col-md-12">
+                            <?php
+                                $image = get_field('boost_img');
+
+                                if (!empty($image)): ?>
+                                    <img 
+                                    src="<?php echo $image['url']; ?>" 
+                                    alt="<?php echo $image['alt']; ?>">
+                                <?php endif;
+                            ?>
+                        </div>
+                        <div class="boost__desc v-col-lg-6 v-col-md-12">
+                            <h1 class="main__title black boost__title"><?php the_field('boost_title'); ?></h1>
+                            <button class="button__trial button__subscribe" type="button"><?php the_field('boost_link'); ?></button>
+                        </div>
+                    </section>
+
+                    <?php
+            }
+
+            wp_reset_postdata(); // сброс
+            ?>
+        </section>
     </main>
 <?php
 get_footer();
--- template-parts/category/category-posts.php
+++ template-parts/category/category-posts.php
@@ -1,46 +1,50 @@
-<section class="popular-posts">
-    <?php
-    $category = get_queried_object();
-    $category_slug = $category->slug;
-    $category_name = $category->name;
-    $category_id = $category->term_id;
+<section class="articles container">
+    <div class="v-row" style="width:100%">
+        <?php
+        $category = get_queried_object();
+        $category_slug = $category->slug;
+        $category_name = $category->name;
+        $category_id = $category->term_id;
 
-    $args = array(
-        'order' => 'desc',
-        'posts_per_page' => -1,
-        'post_type' => 'post',
-        'cat' => $category_id,
-    );
+        $args = array(
+            'order' => 'desc',
+            'posts_per_page' => -1,
+            'post_type' => 'post',
+            'cat' => $category_id,
+        );
 
-    $query = new WP_Query( $args );
+        $query = new WP_Query( $args );
 
-    if ( $query->have_posts() ) {
-        while ( $query->have_posts() ) {
-            $query->the_post(); ?>
-            <article class="popular-post post-card-animation">
-                <div class="popular-post__content">
-                    <div class="popular-post__header">
-                        <span class="category-label category-label--<?= $category_slug; ?> category-label--no-changes">
-                            <?= $category_name?>
-                        </span>
-                        <span class="popular-post__date">
-                            <?php the_time('M j, Y'); ?>
-                        </span>
-                    </div>
-                    <div class="popular-post__body">
-                        <a href="<?php the_permalink(); ?>" class="popular-post__title">
-                            <?php the_title(); ?>
-                        </a>
-                    </div>
-                    <div class="popular-post__footer">
-                        <div class="popular-post__author">
-                            <?php the_author_posts_link(); ?>
+        if ( $query->have_posts() ) {
+            while ( $query->have_posts() ) {
+                $query->the_post(); ?>
+                <article class="article v-col-lg-4 v-col-md-6 v-col">
+                    <div class="article-content">
+                        <div class="article-content__img bg-orange">
+                            <img class="article-content__img-img" src="<?php 
+                                    if(has_post_thumbnail()) {
+                                        the_post_thumbnail_url();
+                                    } else {
+                                        echo get_template_directory_uri() . '/assets/img/not-found.png';
+                                    }
+                                ?>" alt="article-img" loading="lazy">
+                        </div>
+                        <div class="article-content" style="width:100%">
+                            <div class="article-content__header">
+                                <span class="post__time-to-read" style="width:100%"><?php the_time('M j, Y'); ?></span>
+                            </div>
+                            <div class="article-content__body" style="width:100%">
+                                <a href="<?php the_permalink(); ?>" class="post__title"><?php the_title(); ?></a>
+                                <p class="post__text article-content__body-text">
+                                    <?php the_excerpt(); ?>
+                                </p>
+                            </div>
                         </div>
                     </div>
-                </div>
-            </article>
-        <?php }
-    }
-    wp_reset_postdata();
-    ?>
+                </article>
+            <?php }
+        }
+        wp_reset_postdata();
+        ?>
+    </div>
 </section>

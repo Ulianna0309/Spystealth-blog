77770f79c9a4da0b82207ec417d16191701bb342
yulia_vishnevskaya <yulia_vishnevskaya@tchdm.live>
2022-03-28 23:41:06 +0300

    fix

--- 404.php
+++ 404.php
@@ -3,14 +3,13 @@ get_header();
 ?>
 	
 <main id="primary" class="site-main main-content">
-	<div class="container">
-		<a href="<?php echo get_home_url();?>">
-			<h1 class="main-post__title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'spytealth' ); ?></h1>
-		</a>
-
-		<img src="" alt="">
+	<div class="container main-post-event" style="text-align: center;">
+	    <svg style="width:100px;fill:#f2ca68;" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M506.3 417l-213.3-364c-16.33-28-57.54-28-73.98 0l-213.2 364C-10.59 444.9 9.849 480 42.74 480h426.6C502.1 480 522.6 445 506.3 417zM232 168c0-13.25 10.75-24 24-24S280 154.8 280 168v128c0 13.25-10.75 24-23.1 24S232 309.3 232 296V168zM256 416c-17.36 0-31.44-14.08-31.44-31.44c0-17.36 14.07-31.44 31.44-31.44s31.44 14.08 31.44 31.44C287.4 401.9 273.4 416 256 416z"/></svg>
+	    <h1 class="main-post__title"><?php esc_html_e( 'Oops! That page can’t be found.
+            It looks like nothing was found at this location.', 'spytealth' ); ?>
+		</h1>
+		<a href="<?php echo get_home_url();?>" class="button__subscribe" style="display:inline-flex;margin:20px 0;">Go to home?</a>
 	</div>
-
 </main><!-- #main -->
 
 <?php
